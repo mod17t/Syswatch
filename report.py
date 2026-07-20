9b2f3f1d77abfe9414440be34f05272480b94597
Modibo <tmod65926@gmail.com>
2026-07-21 01:01:53 +0200

    feat: horodatage des rapport et correction du bug d'encodage des les rapports

--- report.py
+++ report.py
@@ -1,6 +1,14 @@
+from datetime import datetime
+import os 
 
 def generate_report(current):
-    with open("report.txt", "w") as f:
+    # la variable now va permettre de recuperer la date actuelle au format year-month-day_hours-minutes-seconds 
+    now = datetime.now()
+    #creations du repertoire reports s'il n'existe pas encore
+    os.makedirs("reports", exist_ok=True)
+    #creation du fichier de rapports
+    with open(f"reports/report_{now.strftime("%Y-%m-%d_%H-%M-%S")}.txt", "w", encoding="utf-8") as f:
+        f.write(f"Rapport généré le {now.strftime("%d/%m/%Y")} à {now.strftime("%H:%M:%S")} \n\n")
         f.write(f"La consommation du CPU est : {current['cpu']}  \n")
         f.write(f"La consommation de la RAM est : {current['ram']} \n")
         f.write(f"Les processus qui consomment le plus sont : \n")
